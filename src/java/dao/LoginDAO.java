e88cefbc7d82087a421d250ba60fb6667d4510e0
Aditya <adityagupta.kdb@gmail.com>
2016-04-24 22:58:04 -0500

    Update LoginDAO.java

--- src/java/dao/LoginDAO.java
+++ src/java/dao/LoginDAO.java
@@ -17,14 +17,12 @@ public interface LoginDAO {
     public int signUp(LoginBean aProfile);
     public ArrayList findAll();
     public ArrayList findById(String theModel);   
-    public String findByNumber(int theModel);   
+    public ArrayList findByNumber();   
     public String findByIdLogin(String theModel);
     public String findByPass(String pswd);
     public int updateProfile(LoginBean pro);
     public ArrayList findByIdUpdate(String uid);
-    public int purchase(PixelBean thePixel);
-
-    public int middle(PixelBean thePixel);
+    public void middle();
 
     public String findByDName(String dName);
 
